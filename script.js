1e9744ec5118b697aa439db76b1b820a5d42cdc1
ShajidShahriar <shajidsr12@gmail.com>
2025-06-03 20:27:08 +0600

    add gamebaord module with reset and update functionality

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const Gameboard = (function(){
+
+    // let board = [
+    //     [0,0,0],
+    //     [0,0,0],
+    //     [0,0,0]];
+
+
+    let board = ['','','','','','','','',''] //simpler than 2D
+
+    function resetBoard(){
+        for(let i=0; i<board.length; i++){
+            board[i] = '' ;
+        }
+        console.log("board reset :" ,board)
+    }
+
+    function updateBoard(index,mark){
+        if(index < 0 || index > board.length){
+            return false
+        }
+        if(board[index] === ''){
+            board[index] = mark
+            return true
+        }
+        else{
+            return false
+        }
+
+        console.log("updated board:",board)
+
+    }
+
+    return {
+        resetBoard,
+        updateBoard
+    }
+})();
 
